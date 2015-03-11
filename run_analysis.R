d2cfcc2f1b09e2592db4ffe778e91f65a169b654
Carter Roughton <carter.roughton@tstllc.net>
2015-03-11 16:14:23 -0400

    Work in Progress

--- run_analysis.R
+++ run_analysis.R
@@ -4,19 +4,42 @@ library("dplyr")
 
 setwd(".")
 
+activities <- read.table("./ucidataset/activity_labels.txt")
 features <- as.vector(read.table("./ucidataset/features.txt",head=F,stringsAsFactors=F)[,2])
 
-readY <- function(dataType){
-  DT <- read.table(paste("./ucidataset/",dataType,"/y_",dataType,".txt",sep=""), head=F)
-}
-
-addActivityToX <- function(DT,YDT){
-  DT[,"activity"] <- as.vector(YDT$V1)
+readSubject <- function(dataType){
+  DT <- read.table(paste("./ucidataset/",dataType,"/subject_",dataType,".txt",sep=""), head=F)
   DT
 }
 
-addColumnsToX <- function(DT){
-  colnames(DT) <- features
+readY <- function(dataType){
+  DT <- read.table(paste("./ucidataset/",dataType,"/y_",dataType,".txt",sep=""), head=F)
+  DT
+}
+
+addSubjectColumn <- function(DT,subjectDT){
+  DT <- cbind(subject_id = subjectDT$V1, DT)
+  DT
+}
+
+addActivityColumn <- function(DT,YDT){
+  mappings <- sapply(as.vector(YDT$V1), function(key){
+    activities[key,2]
+  })
+  DT <- cbind(activity = mappings, DT)
+  DT
+}
+
+cleanUpColumnNames <- function(cols){
+  cols <- gsub("\\(","",cols) 
+  cols <- gsub("\\)","",cols)
+  cols <- gsub("-","_",cols)
+  cols <- gsub(",","_",cols)
+  cols
+}
+
+addColumnHeadersToX <- function(DT){
+  colnames(DT) <- cleanUpColumnNames(features)
   DT
 }
 
@@ -25,10 +48,29 @@ readX <- function(dataType){
   DT
 }
 
-buildDataset <- function(dataType){
-  readX("test") %>% addColumnsToX %>% addActivityToX(readY("test"))
+stdOrMeanColumns <- function(DT){
+  cols <- colnames(DT)[grepl("std|mean",colnames(DT))]
+  subset( DT, select=cols)
 }
 
-testDF  <- buildDataset("test")
-trainDF <- buildDataset("train")
+addDataTypeColumn <- function(DT,dataType){
+  DT <- cbind(data_set_type = as.vector(rep(dataType,nrow(DT))),DT)
+  DT
+}
+
+buildDataset <- function(dataType){
+  readX(dataType) %>% 
+    addColumnHeadersToX %>% 
+    stdOrMeanColumns %>% 
+    addDataTypeColumn(dataType) %>% 
+    addActivityColumn(readY(dataType)) %>% 
+    addSubjectColumn(readSubject(dataType))
+}
+
+#testDF  <- buildDataset("test")
+#trainDF <- buildDataset("train")
+
+combined <- bind_rows(buildDataset("test"),buildDataset("train"))
+combinedSorted <- combined[with(combined,order(activity)),]
+
 
